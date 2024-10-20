daf402052d866d52cc4d4abfb6bb131976786a7d
emvasquez4 <emvasquez4@gmail.com>
2024-10-19 19:57:57 -0600

    api publicada

--- GestionCandidatos/wwwroot/gcandidatospersonal/src/services/Api/api.js
+++ GestionCandidatos/wwwroot/gcandidatospersonal/src/services/Api/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-      baseURL: 'https://localhost:44390/api/', //test
-      //baseURL: 'http://dsicandidatosapi.somee.com/api/', //prod
+      // baseURL: 'https://localhost:44390/api/', //test
+      baseURL: 'http://www.dsicandidatosapi.somee.com/api/', //prod
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
