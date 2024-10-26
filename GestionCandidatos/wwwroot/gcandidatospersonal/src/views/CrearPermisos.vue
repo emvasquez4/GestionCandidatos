1dfc03578274ee56aba1d2530201ae849d559c21
IrisArias <ap18018@ues.edu.sv>
2024-10-25 20:06:14 -0600

    permisos en pantallas

--- GestionCandidatos/wwwroot/gcandidatospersonal/src/views/CrearPermisos.vue
+++ GestionCandidatos/wwwroot/gcandidatospersonal/src/views/CrearPermisos.vue
@@ -10,11 +10,46 @@
      import frmPermisos from '../components/Permisos/frmPermisos.vue'
    
      export default {
-     
-   
-       components: {
+      data(){
+        return{
+          nuevo: false,
+          actualizar: false,
+          eliminar: false,
+          consultar: false,
+          pdf: false,
+        }
+      },
+      components: {
         frmPermisos,
         TablaPermisos,
-       },
+      },
+      computed:{
+        ...mapState(['permisos','userId']),
+        ...mapMutations(['setcrearUsuarioState'])
+      },
+      created() {
+        this.getPermisos();
+      },
+      methods:{
+        async getPermisos() {
+          try {
+            await Services.UsuariosRolesService.getUserPermiso('CODUSUARIO', this.userId.toString(), 'PERMISO').
+            then(async response => {
+              const permisos =  response.data
+              this.nuevo = permisos.nuevo;
+              this.actualizar = permisos.actualizar;
+              this.eliminar = permisos.eliminar;
+              this.consultar = permisos.consultar;
+              this.pdf= permisos.pdf;
+            }).catch(error => {
+              // Manejo de errores
+              this.Message = error.response?.data?.message || 'Ocurrió un error al ingresar.';
+              this.showError = true;
+            });
+          } catch (error) {
+              console.error('Error al obtener permisos:', error);
+            }
+        }
+      },
      }
    </script>
--- GestionCandidatos/wwwroot/gcandidatospersonal/src/views/reportes.vue
+++ GestionCandidatos/wwwroot/gcandidatospersonal/src/views/reportes.vue
@@ -8,9 +8,47 @@
   import ReporteEntrevista from '../components/Entrevistas/ReporteEntrevista.vue';
   
   export default {
+    data(){
+        return{
+          nuevo: false,
+          actualizar: false,
+          eliminar: false,
+          consultar: false,
+          pdf: false,
+        }
+      },
     components: {
       ReporteEntrevista
-    }
+    },
+    computed:{
+        ...mapState(['permisos','userId']),
+        ...mapMutations(['setcrearUsuarioState'])
+    },
+    created() {
+    this.getPermisos();
+    },
+    methods:{
+        async getPermisos() {
+          try {
+            await Services.UsuariosRolesService.getUserPermiso('CODUSUARIO', this.userId.toString(), 'USER').
+            then(async response => {
+              const permisos =  response.data
+              this.nuevo = permisos.nuevo;
+              this.actualizar = permisos.actualizar;
+              this.eliminar = permisos.eliminar;
+              this.consultar = permisos.consultar;
+              this.pdf= permisos.pdf;
+            }).catch(error => {
+              // Manejo de errores
+              this.Message = error.response?.data?.message || 'Ocurrió un error al ingresar.';
+              this.showError = true;
+            });
+          } catch (error) {
+              console.error('Error al obtener permisos:', error);
+            }
+        }
+      },
+
   };
   </script>
   
